6d354af544436e58117639d5c456b1389a641d28
Mehra <jatin.mehra@siemens.com>
2021-11-10 20:42:50 +0530

    Add project files.

---
+++ CodingTest/Controllers/PromotionEngineController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodingTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromotionEngineController : ControllerBase
+    {
+        // GET: api/<PromotionEngineController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PromotionEngineController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PromotionEngineController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PromotionEngineController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PromotionEngineController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ XUnitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using CodingTest.Controllers;
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //PromotionEngineController
+            Assert.Equal(1, 1);
+        }
+    }
+}
+
+
